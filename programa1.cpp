f09f95283830774b135ff7f9938d0703aa2944c4
Mariux <mario.lara20019@gmail.com>
2022-08-10 18:49:32 -0600

    primer programa

--- programa1.cpp
+++ programa1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
     int number1 = 0;
-    int number2 = 0;
+    int number2 = 0; //declaracion de la variable (tipo de dato, identificador de la variable) = inicializacion de la variable
 
     cout<<"Emter two integers to compare: ";
     cin>> number1 >>number2;
@@ -22,5 +22,8 @@ int main(){
     }
     if(number1<=number2){
         cout<<number1<<"<="<<number2<<endl;
+    }
+    if(number1>=number2){
+        cout<<number1<<">="<<number2<<endl;
     }
 }
