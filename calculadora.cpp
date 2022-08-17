94d5d5e4e112591abae7bb1008d4f051c8f650e3
Mariux <mario.lara20019@gmail.com>
2022-08-17 11:40:20 -0600

    calculadora

--- calculadora.cpp
+++ calculadora.cpp
@@ -12,12 +12,18 @@ float multi(float a, float b){
 float div(float a, float b){
     return a/b;
 }
+void ingresa(float &a, float &b){
+    cout<<"Ingresa el valor a: ";
+    cin>>a;
+    cout<<"Ingresa el valor b: ";
+    cin>>b;
+}
 
 int main(){
     int num;
-    float a,b, res=0.0;
+    float a, b, res=0.0;
 
-    do
+    /*do
     {
         cout<<"Ingresa el valor a: ";
         cin>>a;
@@ -30,7 +36,6 @@ int main(){
             case 1:
                 res= suma(a,b);
                 cout<<"La suma de a y b es: "<<res<<endl;
-                
                 break;
             case 2:
                 res= resta(a,b);
@@ -51,7 +56,34 @@ int main(){
             default:
                 break;
         }   
-    } while (num!=0);
+    } while (num!=0);*/
+
+    do
+    {
+        cout<<"CALCULADORA"<<endl<<"0. Salir"<<endl<<"1. Suma"<<endl<<"2. Resta"<<endl<<"3. Multiplicacion"<<endl<<"4. Division"<<endl<<"Ingresa que operacion desea realizar: ";
+        cin>>num;
+        if(num==1){
+        ingresa(a,b);
+        res=suma(a,b);
+        cout<<"La suma de a y b es: "<<res<<endl;
+        }
+        if(num==2){
+        ingresa(a,b);
+        res=resta(a,b);
+        cout<<"La resta de a y b es: "<<res<<endl;
+        }
+        if(num==3){
+        ingresa(a,b);
+        res=multi(a,b);
+        cout<<"La multiplicacion de a y b es: "<<res<<endl;
+        }
+        if(num==4){
+        ingresa(a,b);
+        res=div(a,b);
+        cout<<"La division de a y b es: "<<res<<endl;
+        }
+    }while(num!=0);
+    
     
     
 }
